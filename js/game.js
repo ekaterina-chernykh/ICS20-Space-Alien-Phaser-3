a9c13b4c079c0903710c4fc5c73f0c01fc67f738
ekaterina-chern <113055918+ekaterina-chernykh@users.noreply.github.com>
2022-11-11 19:37:17 +0000

    done #02

--- js/game.js
+++ js/game.js
@@ -1,10 +1,27 @@
 /* global Phaser */
 
+// Copyright (c) 2022 Ekaterina all rights reserved
+//
+// Created by Ekaterina
+// Created on: Nov 2022
+// This file contains the JS functions for index.html
+
 const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: true,
+    },
+  },
+  // set background colour
   backgroundColor: 0x5f6e7a
+  scale: {
+  mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
 }
 
 const game = new Phaser.Game(config)
