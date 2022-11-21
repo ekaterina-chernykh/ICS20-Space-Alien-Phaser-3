ab022a1ec6ea0a20c0ace6cb6ed15a187f940fe4
ekaterina-chern <113055918+ekaterina-chernykh@users.noreply.github.com>
2022-11-21 22:35:04 +0000

    done 05

--- js/splashScene.js
+++ js/splashScene.js
@@ -23,11 +23,19 @@ class SplashScene extends Phaser.Scene {
   }
 
   create (data) {
-    this.splashSceneBackgroundImage = this.odd.sprite
+    this.splashSceneBackgroundImage = this.odd.sprite(
+      0,
+      0,
+      "splashSceneBackground"
+    )
+    this.splashSceneBackgroundImage.x = 1920 / 2
+    this.splashSceneBackgroundImage.y = 1080 / 2
   }
 
   update (time, delta){
-    this.scene.switch("titleScene")
+    if (time > 3000){
+      this.scene.switch("titleScene")
+    }
   }
 }
 
