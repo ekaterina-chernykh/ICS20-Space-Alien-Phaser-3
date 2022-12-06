435c16aeb31cbeabbfccc283521ebe4dabf4a031
ekaterina-chern <113055918+ekaterina-chernykh@users.noreply.github.com>
2022-12-06 19:44:55 +0000

    11 done

--- js/gameScene.js
+++ js/gameScene.js
@@ -22,9 +22,12 @@ class GameScene extends Phaser.Scene {
   preload() {
     console.log("Game Scene")
 
+    // images
     this.load.image("starBackground", "./assets/starBackground.png")
     this.load.image("ship", "./assets/spaceShip.png")
     this.load.image("missile", "./assets/missile.png")
+    // sound
+    this.load.audio("laser", "assets/laser1.wav")
   }
 
   create(data) {
@@ -59,12 +62,21 @@ class GameScene extends Phaser.Scene {
       if (this.fireMissile === false) {
         this.fireMissile = true
         const aNewMissile = this.physics.add.sprite(this.ship.x, this.ship.y, "missile")
+        this.missileGroup.add(aNewMissile)
+        this.sound.play("laser")
       }
     }
 
     if (keySpaceObj.isUp === true) {
       this.fireMissile = false
     }
+
+    this.missileGroup.children.each(function(item) {
+      item.y = item.y - 15
+      if (item.y < 0) {
+        item.destroy()
+      }
+    })
   }
 }
 
