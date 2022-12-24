3ff726c35f1abb30e04fb5f51f60c4a3849f661a
ekaterina-chern <113055918+ekaterina-chernykh@users.noreply.github.com>
2022-12-24 00:38:47 +0000

    done 13

--- js/gameScene.js
+++ js/gameScene.js
@@ -39,6 +39,7 @@ class GameScene extends Phaser.Scene {
     this.load.image("alien", "./assets/alien.png")
     // sound
     this.load.audio("laser", "assets/laser1.wav")
+    this.load.audio("explosion", "assets/barrelExploding.wav")
   }
 
   create(data) {
@@ -51,6 +52,14 @@ class GameScene extends Phaser.Scene {
 
     this.alienGroup = this.add.group()
     this.createAlien()
+
+    this.physics.add.collider(this.missileGroup, this.alienGroup, function (missileCollide, alienCollide) {
+      alienCollide.destroy()
+      missileCollide.destroy()
+      this.sound.play("explosion")
+      this.createAlien()
+      this.createAlien()
+    }.bind(this))
   }
 
   update(time, delta) {
